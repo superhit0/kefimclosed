7500bcb110fbc87654b3682a7985557659f91b0c
Abhimanyu Singh <superhit0@gmail.com>
2016-08-21 18:43:26 +0530

    Algo Finish Commit

--- src/main/java/org/manyu/kefimclosed/App.java
+++ src/main/java/org/manyu/kefimclosed/App.java
@@ -1,5 +1,9 @@
 package org.manyu.kefimclosed;
 
+import org.apache.commons.cli.*;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,39 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        AlgoKEFIMClosed obj =new AlgoKEFIMClosed();
+        CommandLine commandLine;
+        Options options = new Options();
+        CommandLineParser parser = new DefaultParser();
+        options.addOption("i", true, "get input file");
+        options.addOption("o", true, "get output file");
+        options.addOption("k", true, "get K");
+        options.addOption("tm", true, "set transaction merging");
+        options.addOption("c", true, "get transaction count");
+        options.addOption("sup", true, "set subtree utility pruning");
+        options.addOption("acp",true,"set closed pattern jump");
+        int k=100;
+        String inputPath="mushroom.txt";
+        String outputPath="abc.txt";
+        boolean activateTransactionMerging=true;
+        int maximumTransactionCount=8124;
+        boolean activateSubtreeUtilityPruning=true;
+        boolean activateClosedPatternJump=true;
+        try {
+            commandLine=parser.parse(options,args);
+            k=Integer.parseInt(commandLine.getOptionValue("k"));
+            inputPath=commandLine.getOptionValue("i");
+            outputPath=commandLine.getOptionValue("o");
+            maximumTransactionCount=Integer.parseInt(commandLine.getOptionValue("c"));
+            activateSubtreeUtilityPruning=Boolean.parseBoolean(commandLine.getOptionValue("sup"));
+            activateTransactionMerging=Boolean.parseBoolean(commandLine.getOptionValue("tm"));
+            activateClosedPatternJump=Boolean.parseBoolean(commandLine.getOptionValue("acp"));
+            obj.runAlgorithm(k,1,inputPath,outputPath,activateTransactionMerging,maximumTransactionCount,activateSubtreeUtilityPruning,activateClosedPatternJump);
+            obj.printStats();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
